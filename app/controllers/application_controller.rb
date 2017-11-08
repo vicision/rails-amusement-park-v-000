88c4052dc81fc1419602b9c1eefbbaebc6a92de5
vicision <duncan.j.mclachlan@gmail.com>
2017-11-07 23:18:59 -0500

    Half tests passed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,28 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
 
+    # before_action :login_required
+
+    # private
+    
+    # def login(user)
+    #   session[:user_id] = user.id # Logging them in as a user.
+    # end
+
+    # def login_required
+    #   if !logged_in?
+    #     redirect_to login_path, :notice => "Please login first!"
+    #   end
+    # end
+
+    # def logged_in?
+    #   !!current_user
+    # end
+    # helper_method :logged_in?
+
+    # def current_user
+    #   @current_user ||= User.find(session[:user_id]) if session[:user_id].present?
+    # end
+    # helper_method :current_user
 
 end
